06576193afdd7e854ca78cdf5044a16e78622b61
neronmoon <alistar.neron@gmail.com>
2016-11-29 08:29:55 +0200

    ls dir

--- build.py
+++ build.py
@@ -9,8 +9,10 @@ projects = [
 cmake_generator = environ['CMAKE_GENERATOR']
 
 for project in projects:
+    print "############# Building %s #############" % project
+    check_call('md arts/%s' % project, shell=True)
     call = partial(check_call, shell=True, cwd=project)
     call('cmake -G "%s" .' % cmake_generator)
     call('msbuild ALL_BUILD.vcxproj /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"')
-    call('msbuild PACKAGE.vcxproj /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"')
-
+    call('dir')
+    # call('Push-AppveyorArtifact mypackage.nupkg')
