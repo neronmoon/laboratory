368f031951d368d75c1b1dc6284c11d550002464
neronmoon <alistar.neron@gmail.com>
2016-11-29 08:37:00 +0200

    publish arts

--- build.py
+++ build.py
@@ -1,17 +1,18 @@
 from subprocess import check_call
 from os import environ
 from functools import partial
+import glob
 
-projects = [
-    'OOP/lab1',
-]
+projects = {
+    'lab1': 'OOP/lab1',
+}
 
 cmake_generator = environ['CMAKE_GENERATOR']
 
-for project in projects:
-    print "############# Building %s #############" % project
-    call = partial(check_call, shell=True, cwd=project)
+for name, path in projects.iteritems():
+    print "############# Building %s #############" % name
+    call = partial(check_call, shell=True, cwd=path)
     call('cmake -G "%s" .' % cmake_generator)
     call('msbuild ALL_BUILD.vcxproj /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"')
-    call('dir')
-    # call('Push-AppveyorArtifact mypackage.nupkg')
+    for artifact in glob('%s.*' % name):
+        call('Push-AppveyorArtifact %s' % artifact)
