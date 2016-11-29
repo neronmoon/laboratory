21047b440abd0181e412ca2e9c7d416786dbf499
neronmoon <alistar.neron@gmail.com>
2016-11-29 08:30:28 +0200

    ls dir

--- build.py
+++ build.py
@@ -10,7 +10,6 @@ cmake_generator = environ['CMAKE_GENERATOR']
 
 for project in projects:
     print "############# Building %s #############" % project
-    check_call('md arts/%s' % project, shell=True)
     call = partial(check_call, shell=True, cwd=project)
     call('cmake -G "%s" .' % cmake_generator)
     call('msbuild ALL_BUILD.vcxproj /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"')
