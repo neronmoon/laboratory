a2f496a556de39e961fda0cf7519866be36c08a9
neronmoon <alistar.neron@gmail.com>
2016-12-11 12:47:01 +0200

    lab 1 comants added

--- OOP/lab1/main.cpp
+++ OOP/lab1/main.cpp
@@ -1,43 +1,53 @@
+// Полдключаем библиотеки
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+// подключаем заголовок нашего класса
 #include "main.h"
 
+// мне не хочется писать std::<<cout так что пусть я сразу буду находится в std окружении
 using namespace std;
 
+
+// Реализация конструктора без аргументов
 Country::Country() {
+    // Присваиваем значение this->name = <country_name>
     strcpy(name, "<country name>");
     strcpy(polity, "<country polity>");
+    // по умолчанию у несуществующей страны нет площади
     area = 0.f;
 }
 
+// Реализация конструктора с аргументами
 Country::Country(char *name, char *polity, float area) {
     strcpy(this->name, name);
     strcpy(this->polity, polity);
     this->area = area;
 }
 
+// Реализация метода вывода
 void Country::Print() {
     cout << "Country " << name << " has " << polity << " polity and area " << area << endl;
 }
 
+// Реализация метода ввода
 void Country::Input() {
     cin >> name >> polity >> area;
 }
 
 
 int main() {
-    Country c1("Russia", "Republican", 99999.f);
-    c1.Print();
+    Country c1("Russia", "Republican", 99999.f); // Создаем "статический" объект с1
+    c1.Print(); // -> Country Russia has Repoblican polity and area 99999
 
-    Country *c2 = new Country;
-    c2->Print();
-    delete (c2);
+    Country *c2 = new Country; // создаем динамический объект с2
+    c2->Print(); // -> Country <country name> has <country polity> polity and area 0
+    delete (c2); // удаляем объект
 
-    Country c3;
-    cout << "Enter new country" << endl;
-    c3.Input();
-    c3.Print();
+    Country c3; // создаем статический объект с3
+    cout << "Enter new country" << endl; // Пишем в консоль чего ожидаем от пользователя
+    c3.Input(); // Вводим данные
+    c3.Print(); // выводим данные
 
-    return 0;
+    return 0; // мы завершились успешно!
 }
--- OOP/lab1/main.h
+++ OOP/lab1/main.h
@@ -1,4 +1,9 @@
+//Это заголовочный файл
+
+// объявляем класс Страна
 class Country {
+    // объявляем методы и констукторы
+    // По-умолчанию они и так были бы public, но почему бы и не написать
 public:
     Country(char *name, char *polity, float area);
 
@@ -7,7 +12,7 @@ public:
     void Print();
     void Input();
 
-private:
+private: // Приватные свойства. Здесь хранятся данные
     char name[25];
     char polity[25];
     float area;
