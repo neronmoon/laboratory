ece56e657669d3b6d90edbaf82bd7f188c232c42
neronmoon <alistar.neron@gmail.com>
2017-01-04 14:55:08 +0200

    build all the things!

--- build.py
+++ build.py
@@ -1,17 +1,16 @@
 from subprocess import check_call
-from os import environ
+from os import environ, listdir
 from functools import partial
 
-projects = [
-    'OOP/lab1',
-]
-
-cmake_generator = environ['CMAKE_GENERATOR']
+projects = map(
+    lambda p: 'OOP/' + p,
+    filter(lambda d: d.startswith('lab'), listdir('OOP'))
+)
 
 for project in projects:
     print "############# Building %s #############" % project
     call = partial(check_call, shell=True, cwd=project)
-    call('cmake -G "%s" .' % cmake_generator)
+    call('cmake -G "%s" .' % environ['CMAKE_GENERATOR'])
     call('msbuild ALL_BUILD.vcxproj /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"')
     ar_name = project.replace('/', '_') + '.zip'
     call('7z a %s *' % ar_name)
