3234c4d507d6d6b1834b19442ee4eac582a79023
neronmoon <alistar.neron@gmail.com>
2017-01-04 15:19:44 +0200

    compress only

--- build.py
+++ build.py
@@ -13,6 +13,6 @@ for project in projects:
     call('cmake -G "%s" .' % environ['CMAKE_GENERATOR'])
     call('msbuild ALL_BUILD.vcxproj /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"')
     ar_name = project.replace('/', '_') + '.zip'
-    for ext in ['doc', 'docx', 'h', 'cpp', 'exe']:
-        call('7z a %s *.%s' % (ar_name, ext))
+    for mask in ['*.doc', '*.docx', '*.h', '*.cpp', 'Release/*.exe']:
+        call('7z a %s %s' % (ar_name, mask))
     call('appveyor PushArtifact %s' % ar_name)
